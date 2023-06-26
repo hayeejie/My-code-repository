198c2cc9290b85f8a02a5f24c20396e1a60db9e4
hayeejie <1287150696@qq.com>
2023-06-26 10:23:04 +0800

    chore: 🔧 复制大文件

---
+++ CODE_Java/basic-code/test8/src/com/hayeejie/iotest/CopyBigFile.java
@@ -0,0 +1,31 @@
+package com.hayeejie.iotest;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * Hard learning
+ *
+ * @author hayeejie
+ * @name Learn-basic-code
+ * @date 2023/6/25 14:48
+ */
+public class CopyBigFile {
+    public static void main(String[] args) throws IOException {
+        //计算复制时间
+        long start = System.currentTimeMillis();
+        FileInputStream fis = new FileInputStream("E:\\2023-05-01 21-45-36.mkv");
+        FileOutputStream fos = new FileOutputStream("E:\\2023-05-01 21-45-36copy.mkv");
+        byte[] bytes = new byte[1024 * 1024 * 10];
+        int len;
+        while ((len = fis.readNBytes(bytes, 0, bytes.length)) != 0) {
+            fos.write(bytes, 0, len);
+        }
+        fos.close();
+        fis.close();
+        long stop = System.currentTimeMillis();
+        long time = stop - start;
+        System.out.println(time);
+    }
+}
